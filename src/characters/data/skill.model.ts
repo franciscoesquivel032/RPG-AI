88f2781bb996e073acddf2bb5bf6193b07b9656b
Francisco Esquivel Rico <franciscoesquivel032@gmail.com>
2025-06-13 16:45:25 +0200

    Comentaries *.*

--- src/characters/data/skill.model.ts
+++ src/characters/data/skill.model.ts
@@ -19,6 +19,7 @@ export class Skill extends Model {
     @Column
     declare effect: string;
 
+    // Association with Character
     @ForeignKey(() => Character)
     @Column
     declare characterId: number;
