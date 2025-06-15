dc05b6a13cc763c0bddb13bda5fadc0cc4b05d15
Francisco Esquivel Rico <franciscoesquivel032@gmail.com>
2025-06-15 14:28:00 +0200

    validations added to models

--- src/characters/dtos/CharacterDto.ts
+++ src/characters/dtos/CharacterDto.ts
@@ -1,17 +1,25 @@
 import { Expose } from "class-transformer";
+import { IsInt, IsString } from "class-validator";
+import { IsIn } from "sequelize-typescript";
 
 export class CharacterDto {
     @Expose()
+    @IsString()
     name: string;
     @Expose()
+    @IsString()
     class: string;
     @Expose()
+    @IsString()
     lore: string;
     @Expose()
+    @IsString()
     skinDescription: string;
     @Expose()
+    @IsInt()
     passiveSkill: number;
     @Expose()
+    @IsInt()
     location: number;
 
     constructor(name: string, className: string, lore: string, skinDescription: string, passiveSkill: number, location: number) {
--- src/locations/dtos/locationDto.ts
+++ src/locations/dtos/locationDto.ts
@@ -1,8 +1,23 @@
+import { Expose } from "class-transformer";
+import { IsInt, IsString, IsArray } from "class-validator";
+
+
 export class LocationDto {
+    @Expose()
+    @IsInt()
     id: number;
+    @Expose()
+    @IsString()
     name: string;
+    @Expose()
+    @IsString()
     description: string;
+    @Expose()
+    @IsString()
     lore: string;
+    @Expose()
+    @IsArray()
+    @IsInt({ each: true})
     characters?: number[];
 
     constructor(id: number, name: string, description: string, lore: string, characters?: number[]) {
