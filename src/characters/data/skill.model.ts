36da3d260295b8d1df05bb18c06954cc421f1e3a
Francisco Esquivel Rico <franciscoesquivel032@gmail.com>
2025-06-15 14:28:55 +0200

    validations added to skill model

--- src/characters/data/skill.model.ts
+++ src/characters/data/skill.model.ts
@@ -1,4 +1,4 @@
-import { BelongsTo, HasOne, Model } from 'sequelize-typescript';
+import { BelongsTo, HasOne, IsInt, Model } from 'sequelize-typescript';
 import { AutoIncrement, Column, ForeignKey, PrimaryKey, Table } from "sequelize-typescript";
 import { Character } from "./character.model";
 import { on } from 'events';
--- src/characters/dtos/SkillDto.ts
+++ src/characters/dtos/SkillDto.ts
@@ -1,11 +1,15 @@
 import { Expose } from "class-transformer";
+import { IsString } from "class-validator";
 
 export class SkillDto {
   @Expose()
+  @IsString()
   name: string;
   @Expose()
+  @IsString()
   description: string;
   @Expose()
+  @IsString()
   effect: string;
 
   constructor(name: string, description: string, effect: string) {
